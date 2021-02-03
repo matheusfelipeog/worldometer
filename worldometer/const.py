f93fd2742d278fc97298f1920d0a373225e6708d
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2021-02-03 20:47:32 -0300

    Rename constant variable

--- worldometer/const.py
+++ worldometer/const.py
@@ -1,4 +1,4 @@
-worldometer_url = 'https://www.worldometers.info/'
+url = 'https://www.worldometers.info/'
 
 css_selector_of_counter_numbers = '.counter-number'
 css_selector_of_counter_item = '.counter-item, .counter-item-double'
--- worldometer/models.py
+++ worldometer/models.py
@@ -1,11 +1,11 @@
 from requests_html import HTMLSession 
 
-from .const import worldometer_url
+from .const import url
 
 class Worldometer(object):
     def __init__(self):
         self._session = HTMLSession()
 
         # Get html page and render dynamic content
-        self._r = self._session.get(worldometer_url)
+        self._r = self._session.get(url)
         self._r.html.render()
