0812fd46ec9c3e3dd4c7faf9e47ac98d1a60e146
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2021-02-06 20:25:03 -0300

    Change internal attribute to simple variable

--- worldometer/models.py
+++ worldometer/models.py
@@ -4,11 +4,11 @@ from .const import URL
 
 class Worldometer(object):
     def __init__(self):
-        self._session = HTMLSession()
+        session = HTMLSession()
 
         try:
             # Get html page and render dynamic content
-            self._r = self._session.get(URL, timeout=15)
+            self._r = session.get(URL, timeout=15)
             self._r.html.render()
 
         except Exception as err:
