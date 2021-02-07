408f500e470339f592f4e8499508f0b44a3dd4b6
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2021-02-07 00:19:34 -0300

    Implement find and sanitize data method

--- worldometer/models.py
+++ worldometer/models.py
@@ -1,11 +1,14 @@
+import re
+
 from requests_html import HTMLSession
 
 from .const import URL
+from .const import CSS_SELECTOR_OF_COUNTER_NUMBERS
 
 
 class Worldometer(object):
     def __init__(self):
-        self._r = Worldometer._get(URL)
+        self._r = self._sanitize_data(self._get(URL))
         
     @staticmethod
     def _get(url):
@@ -17,7 +20,27 @@ class Worldometer(object):
             r = session.get(url, timeout=15)
             r.html.render()
 
-            return r
+            return Worldometer._find_data(r)
 
         except Exception as err:
             raise Exception(err)
+
+    @staticmethod
+    def _find_data(r):
+        content = r.html.find(CSS_SELECTOR_OF_COUNTER_NUMBERS)
+
+        return content
+
+    @staticmethod
+    def _sanitize_data(data_list):
+        sanitized_data = []
+
+        for data in data_list:
+            
+            # Get only the number and convert to int
+            found = re.search(r'[0-9,]+', data.text).group()
+            number = int(found.replace(',', ''))
+
+            sanitized_data.append(number)
+        
+        return sanitized_data
