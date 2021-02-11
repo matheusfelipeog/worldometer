f839b29528c5370937d3b5db2cc20948c8e9d023
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2021-02-10 23:47:43 -0300

    Add docstring of usage Worldometer

--- worldometer/core.py
+++ worldometer/core.py
@@ -5,6 +5,58 @@ worldometer.core
 ----------------
 
 This module contains the core objects that power Worldometer.
+
+Usage Worldometer:
+
+>>> from worldometer import Worldometer
+>>> w = Worldometer()
+
+>>> w.what_is_here()
+{'categories': 8, 'labels': 63, 'metrics': 63}
+
+>>> w.categories()
+[   
+    'world_population',
+    'government_and_economics',
+    'society_and_media',
+    ...
+]
+
+>>> w.metrics_labels()
+[   
+    'current_world_population',
+    'births_this_year',
+    'births_today',
+    'deaths_this_year',
+    'deaths_today',
+    'net_population_growth_this_year',
+    ...
+]
+
+>>> w.metrics
+[   
+    7845087963,
+    15741371,
+    5676,
+    6608605,
+    2383,
+    9132766,
+    ...
+]
+
+>>> w.metrics_with_labels()
+{   
+    'abortions_this_year': 4785492,
+    'bicycles_produced_this_year': 17070566,
+    'births_this_year': 15741371,
+    'births_today': 5676,
+    'blog_posts_written_today': 110171,
+    'cars_produced_this_year': 8999185,
+    'cellular_phones_sold_today': 98846,
+    ...: ...
+}
+
+More info in: github.com/matheusfelipeog/worldometer
 """
 
 
