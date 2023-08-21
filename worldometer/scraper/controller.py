217b858de7d3853d2abf88a0c96dd93849df216f
Matheus Felipe <matheusfelipeog@protonmail.com>
2023-08-21 16:33:53 -0300

    refactor: define base_url const of worldometers.info

--- worldometer/scraper/controller.py
+++ worldometer/scraper/controller.py
@@ -3,12 +3,13 @@ from typing import Dict, Optional
 from worldometer.scraper.browser import Browser
 from worldometer.scraper.parser import get_rts_counters_only_with_last_value_key
 
+BASE_URL = 'https://www.worldometers.info'
 
 browser = Browser()
 
 
 def get_rts_counters_object() -> Dict[str, Optional[int]]:
-    html = browser.get_page_content('https://www.worldometers.info')
+    html = browser.get_page_content(BASE_URL)
     script_return = browser.run_js_script(html, script='() => rts_counters')
     rts_counters = get_rts_counters_only_with_last_value_key(rts_counters=script_return)
     return rts_counters
