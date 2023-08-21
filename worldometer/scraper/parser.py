dfb6b96963514eb553531543ee45167185947cde
Matheus Felipe <matheusfelipeog@protonmail.com>
2023-08-21 16:01:51 -0300

    fix: define type hint of rts_counters parser

--- worldometer/scraper/parser.py
+++ worldometer/scraper/parser.py
@@ -1,5 +1,5 @@
-from typing import Dict
+from typing import Dict, Optional
 
 
-def get_rts_counters_only_with_last_value_key(rts_counters: Dict[str, dict]):
+def get_rts_counters_only_with_last_value_key(rts_counters: Dict[str, dict]) -> Dict[str, Optional[int]]:
     return {key: val.get('last_value') for key, val in rts_counters.items()}
