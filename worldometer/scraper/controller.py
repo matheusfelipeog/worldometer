73ba6ee6e9c0f16e8594ca87c27b5efef57f902c
Matheus Felipe <matheusfelipeog@protonmail.com>
2023-08-21 17:58:09 -0300

    feat: create utils module and make_url function to use in controller

--- worldometer/scraper/controller.py
+++ worldometer/scraper/controller.py
@@ -3,13 +3,16 @@ from typing import Dict, Optional
 from worldometer.scraper.browser import Browser
 from worldometer.scraper.parser import get_rts_counters_only_with_last_value_key
 
+from worldometer.scraper.utils import make_url
+
 BASE_URL = 'https://www.worldometers.info'
 
 browser = Browser()
 
 
 def get_rts_counters_object() -> Dict[str, Optional[int]]:
-    html = browser.get_page_content(BASE_URL)
+    url = make_url(BASE_URL)
+    html = browser.get_page_content(url)
     script_return = browser.run_js_script(html, script='() => rts_counters')
     rts_counters = get_rts_counters_only_with_last_value_key(rts_counters=script_return)
     return rts_counters
