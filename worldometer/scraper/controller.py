f365b4ad0498652c02138a826e5a22c15ca4e463
Matheus Felipe <matheusfelipeog@protonmail.com>
2023-08-26 01:34:25 -0300

    feat: implement data table controller

--- worldometer/scraper/controller.py
+++ worldometer/scraper/controller.py
@@ -1,7 +1,8 @@
-from typing import Dict, Optional
+from typing import Dict, List, Optional
 
 from worldometer.scraper.browser import Browser
 from worldometer.scraper.parser import get_rts_counters_only_with_last_value_key
+from worldometer.scraper.parser import get_html_table_data
 
 from worldometer.scraper.utils import make_url
 
@@ -16,3 +17,17 @@ def get_rts_counters_object() -> Dict[str, Optional[int]]:
     script_return = browser.run_js_script(html, script='() => rts_counters')
     rts_counters = get_rts_counters_only_with_last_value_key(rts_counters=script_return)
     return rts_counters
+
+
+def get_data_table(
+    path_url: str,
+    new_headers: List[str],
+    table_position: int,
+    render: bool = False
+) -> List[dict]:
+    url = make_url(BASE_URL, path_url)
+    html = browser.get_page_content(url)
+    if render:
+        browser.render_page(html)
+    data = get_html_table_data(html.html, new_headers, table_position)
+    return data
