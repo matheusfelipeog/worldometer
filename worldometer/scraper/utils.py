63f4bc0b94b189d107a722ee1a697263eb832d4e
Matheus Felipe <matheusfelipeog@protonmail.com>
2023-08-27 21:05:35 -0300

    refactor: support make_url for a root route (url/)

--- worldometer/scraper/utils.py
+++ worldometer/scraper/utils.py
@@ -5,6 +5,6 @@ from typing import Optional
 
 def make_url(base_url: str, path_url: Optional[str] = None) -> str:
     url = base_url
-    if path_url and re.match(r'^/{1}.+', path_url):
+    if path_url and re.match(r'^/{1}.*', path_url):
         url += path_url
     return url
