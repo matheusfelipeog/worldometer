651255de26b82fa799b3eebfdeb0cb943357efe6
Matheus Felipe <matheusfelipeog@protonmail.com>
2023-08-28 00:33:01 -0300

    feat: implement load data in CountryCodes

--- worldometer/world/country_codes.py
+++ worldometer/world/country_codes.py
@@ -2,6 +2,8 @@ from copy import deepcopy
 from dataclasses import dataclass
 from typing import List
 
+from worldometer.scraper.controller import get_data_tables
+
 
 @dataclass
 class CountryCodesData:
@@ -15,12 +17,29 @@ class CountryCodesData:
 class CountryCodes:
 
     source_path = '/country-codes'
+    table_position = 0
+    column_names = (
+        'country',
+        'calling_code',
+        'three_letter_iso',
+        'two_letter_iso',
+        'three_digit_iso_numeric'
+    )
 
     def __init__(self) -> None:
         self._data = self._load_data()
 
     def _load_data(self) -> List[CountryCodesData]:
-        return []
+        dts = get_data_tables(
+            path_url=self.source_path,
+            new_column_names=[
+                self.column_names
+            ]
+        )
+        return [
+            CountryCodesData(**data_line)
+            for data_line in dts[self.table_position]
+        ]
 
     def data(self) -> List[CountryCodesData]:
         return deepcopy(self._data)
