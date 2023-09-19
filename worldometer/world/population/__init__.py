2220af564b6b78d661ea94ec2acadddb22d0007b
Matheus Felipe <matheusfelipeog@protonmail.com>
2023-09-18 22:32:33 -0300

    feat: implement api to get largest cities in the world

--- worldometer/world/population/__init__.py
+++ worldometer/world/population/__init__.py
@@ -1,7 +1,9 @@
 __all__ = [
     'CountriesByPopulation',
+    'LargestCities',
     'MostPopulousCountries'
 ]
 
 from worldometer.world.population.countries_by_population import CountriesByPopulation
+from worldometer.world.population.largest_cities import LargestCities
 from worldometer.world.population.most_populous_countries import MostPopulousCountries
