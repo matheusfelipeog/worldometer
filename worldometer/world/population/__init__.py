af3564f4518de541b2b99e707c993e9b932c6c56
Matheus Felipe <matheusfelipeog@protonmail.com>
2023-09-18 22:41:59 -0300

    feat: implement api to get world population projections

--- worldometer/world/population/__init__.py
+++ worldometer/world/population/__init__.py
@@ -1,9 +1,11 @@
 __all__ = [
     'CountriesByPopulation',
     'LargestCities',
-    'MostPopulousCountries'
+    'MostPopulousCountries',
+    'WorldPopulationProjections'
 ]
 
 from worldometer.world.population.countries_by_population import CountriesByPopulation
 from worldometer.world.population.largest_cities import LargestCities
 from worldometer.world.population.most_populous_countries import MostPopulousCountries
+from worldometer.world.population.world_population_projections import WorldPopulationProjections
