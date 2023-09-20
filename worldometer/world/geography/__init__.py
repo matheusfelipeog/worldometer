92041c4a2676e02343af682afb40c7ea7768e936
Matheus Felipe <matheusfelipeog@protonmail.com>
2023-09-20 16:46:29 -0300

    feat: create geography package and implement new APIs
    Implemented APIs to get countries from all regions of the world:
        - World Countries;
        - Asia Countries;
        - Africa Countries;
        - Europe Countries;
        - Latin American And The Caribbean Countries;
        - Northern American Countries;
        - Oceania Countries.

---
+++ worldometer/world/geography/__init__.py
@@ -0,0 +1,19 @@
+__all__ = [
+    'WorldCountries',
+    'AsiaCountries',
+    'AfricaCountries',
+    'EuropeCountries',
+    'LatinAmericanAndTheCaribbeanCountries',
+    'NorthernAmericanCountries',
+    'OceaniaCountries'
+]
+
+from worldometer.world.geography.countries import (
+    WorldCountries,
+    AsiaCountries,
+    AfricaCountries,
+    EuropeCountries,
+    LatinAmericanAndTheCaribbeanCountries,
+    NorthernAmericanCountries,
+    OceaniaCountries
+)
