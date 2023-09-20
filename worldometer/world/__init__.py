cd891ce49c9faa2397ce913fccfbcb50b8b66bae
Matheus Felipe <matheusfelipeog@protonmail.com>
2023-09-20 18:36:35 -0300

    feat: define geography package as a public api

--- worldometer/world/__init__.py
+++ worldometer/world/__init__.py
@@ -1,9 +1,11 @@
 __all__ = [
     'WorldCounters',
     'CountryCodes',
-    'population'
+    'population',
+    'geography'
 ]
 
 from worldometer.world.counters import WorldCounters
 from worldometer.world.country_codes import CountryCodes
 from worldometer.world import population
+from worldometer.world import geography
