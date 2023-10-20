9cbf6e324d351587d568113c1312c6e4aaefc883
Matheus Felipe <matheusfelipeog@protonmail.com>
2023-10-20 00:13:30 -0300

    chore: add sphinx.ext.duration extension in conf.py file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,8 @@ sys.path.insert(0, os.path.abspath('../..'))
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'sphinx.ext.duration'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
